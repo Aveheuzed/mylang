7dc73c7689abd49df6075991ac45bad963a0be59
Aveheuzed <a.masson555@ntymail.com>
2021-01-25 22:33:54 +0100

    Implemented the machinery underlying variables.
    - Hashing and interning identifiers;
    - Namespace access/insertion.
    The identifiers are interned to simplify and speed up the operations on namespaces.
    (No hashing is necessary then.)

---
+++ src/namespace.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+#include "namespace.h"
+
+#define GROW_THRESHHOLD (0.8) // TODO: benchmark this value
+
+#define BUCKET_FULL(bucket) ((bucket).key != NULL)
+
+static void raw_ns_set_value(Namespace *const pool, Variable v) {
+        const size_t mask = pool->len-1;
+        hash_t index = v.hash & mask;
+        for (;
+                BUCKET_FULL(pool->pool[mask&index]) &&
+                (pool->pool[mask&index].key != v.key);
+                index++);
+        pool->pool[mask&index] = v;
+        pool->nb_entries++;
+}
+
+static Namespace* growNamespace(Namespace* pool) {
+        const size_t new_size = pool->len * 2;
+        Namespace *new_pool = calloc(offsetof(Namespace, pool) + sizeof(Variable)*new_size, 1);
+        new_pool->len = new_size;
+        for (size_t i=0; i<pool->len; i++) {
+                Variable v = pool->pool[i];
+                if (v.key != NULL) raw_ns_set_value(new_pool, v);
+        }
+        freeNamespace(pool);
+        return new_pool;
+}
+
+Namespace* allocateNamespace(void) {
+        Namespace *const pool = calloc(offsetof(Namespace, pool) + sizeof(Variable)*1, 1);
+        pool->len = 1;
+        return pool;
+}
+void freeNamespace(Namespace* pool) {
+        free(pool);
+}
+
+void ns_set_value(Namespace **const pool, char *const key, Object value) {
+        if ((*pool)->len * GROW_THRESHHOLD <= (*pool)->nb_entries)
+                *pool = growNamespace(*pool);
+        raw_ns_set_value(*pool, (Variable){.key=key, .value=value});
+}
+
+Object* ns_get_value(Namespace *const pool, const char* key) {
+        const hash_t mask = pool->len - 1;
+        hash_t index = mask & (hash_t) key;
+        Variable* v;
+        do {
+                v = &(pool->pool[(index++)&mask]);
+                if (v->key == key) return &(v->value);
+                if (!BUCKET_FULL(*v)) return NULL;
+        } while (1);
+}
+
+#undef BUCKET_FULL
+#undef GROW_THRESHHOLD
---
+++ src/namespace.h
@@ -0,0 +1,33 @@
+#ifndef identifiers_pool_h
+#define identifiers_pool_h
+
+#include <stddef.h>
+
+#include "hash.h"
+#include "object.h"
+
+typedef struct Variable {
+        /* Since identifiers are internalized, we use the pointer to the string
+        as hash digest for the identifier.
+        We can therefore guarantee no collisions.
+        Furthermore, the hashing is free! */
+        union {
+                char* key;
+                hash_t hash;
+        };
+        Object value;
+} Variable;
+
+typedef struct Namespace {
+        size_t len; // guaranteed to be a power of 2
+        size_t nb_entries;
+        Variable pool[];
+} Namespace;
+
+Namespace* allocateNamespace(void);
+void freeNamespace(Namespace* pool);
+
+void ns_set_value(Namespace **const pool, char *const key, Object value);
+Object* ns_get_value(Namespace *const pool, const char* key);
+
+#endif
