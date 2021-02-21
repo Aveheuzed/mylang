fa0d9199b9b10c8d1d67f500ef1e0b92d5f86819
Aveheuzed <a.masson555@ntymail.com>
2021-02-21 16:02:28 +0100

    Logs and errors are displayed on stderr, rather than stdout.

--- headers/utils/debug.h
+++ headers/utils/debug.h
@@ -3,7 +3,7 @@
 
 #ifdef DEBUG
         #include <stdio.h>
-        #define LOG(text, ...) printf("[%-22s] " text ".\n", __func__ __VA_OPT__(,) __VA_ARGS__)
+        #define LOG(text, ...) fprintf(stderr, "[%-22s] " text ".\n", __func__ __VA_OPT__(,) __VA_ARGS__)
 #else
         #define LOG(text, ...)
 #endif
--- src/utils/error.c
+++ src/utils/error.c
@@ -3,14 +3,14 @@
 #include "headers/utils/error.h"
 
 void TypeError(const Token t) {
-        printf("Incompatible types for %s.\n", t.source);
+        fprintf(stderr, "Incompatible types for %s.\n", t.source);
 }
 void SyntaxError(const Token t) {
-        printf("Invalid syntax on line %u at \"%*s\".\n", t.line, t.length, t.source);
+        fprintf(stderr, "Invalid syntax on line %u at \"%*s\".\n", t.line, t.length, t.source);
 }
 void RuntimeError(const Token t) {
-        printf("Runtime error on line %u at \"%*s\".\n", t.line, t.length, t.source);
+        fprintf(stderr, "Runtime error on line %u at \"%*s\".\n", t.line, t.length, t.source);
 }
 void ArityError(const uintptr_t expected, const uintptr_t actual) {
-        printf("Arity error : expected %lu argument(s), got %lu.\n", expected, actual);
+        fprintf(stderr, "Arity error : expected %lu argument(s), got %lu.\n", expected, actual);
 }
