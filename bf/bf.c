cf758dd5433e76addc3f67452beb08355a5c7918
Aveheuzed <a.masson555@ntymail.com>
2021-02-24 19:48:11 +0100

    A nice vanilia bf machine — very fragile…
    It crashes if it encounters anything but `+-.,<>[]`, or if it `<` at
    the first cell. It does allow incrementing above 0xff, or decrementing
    under 0, which I dont want to.
    This is for the sake of simplicity, and also because I plan to use it as
    bytecode VM later, so it will always be given clean code anyway.
    
    The memory, however, is managed, and the tape can grow (on the right) as
    needed.

---
+++ bf/bf.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+typedef uint8_t Word;
+const Word LIM = UINT8_MAX;
+
+inline Word* growBand(Word* ptr, const size_t oldlen, const size_t newlen) {
+        ptr = reallocarray(ptr, newlen, sizeof(Word));
+        memset(ptr+oldlen, 0, (newlen-oldlen)*sizeof(Word));
+        return ptr;
+}
+
+void interpretBF(char const* text) {
+        static const void* labels[] = {
+                ['<'] = &&rsh,
+                ['>'] = &&lsh,
+                ['+'] = &&inc,
+                ['-'] = &&dec,
+                ['.'] = &&out,
+                [','] = &&in,
+                ['['] = &&lbracket,
+                [']'] = &&rbracket,
+                ['\0'] = &&end,
+        };
+
+        #define NEXT() goto *labels[*++text]
+
+        size_t pos = 0;
+        size_t len = 1;
+        Word* data = growBand(NULL, 0, 1);
+
+        goto *labels[*text];
+
+        inc:
+                data[pos]++;
+                NEXT();
+        dec:
+                data[pos]--;
+                NEXT();
+        rsh:
+                pos--;
+                NEXT();
+        lsh:
+                pos++;
+                if (pos>=len) {
+                        data = growBand(data, len, len*2);
+                        len*=2;
+                }
+                NEXT();
+        in:
+                data[pos] = getchar();
+                NEXT();
+        out:
+                putchar(data[pos]);
+                NEXT();
+        lbracket:
+                if (!data[pos]) {
+                        size_t stack = 0;
+                        for(text++; (*text != ']' || stack != 0); text++) {
+                                if (*text == '[') stack++;
+                                if (*text == ']') stack--;
+                        }
+                }
+                NEXT();
+        rbracket:
+                if (data[pos]) {
+                        size_t stack = 0;
+                        for(text--; (*text != '[' || stack != 0); text--) {
+                                if (*text == '[') stack--;
+                                if (*text == ']') stack++;
+                        }
+                }
+                NEXT();
+
+        end:
+        free(data);
+
+        #undef NEXT
+}
+
+
+int main(void) {
+        FILE* file = fopen("mandelbrot_cured.b", "r");
+        char* text = malloc(12000);
+        text[fread(text, 1, 12000, file)] = '\0';
+        fclose(file);
+
+        interpretBF(text);
+}
